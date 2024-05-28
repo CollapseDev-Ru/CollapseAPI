d6f81773b5ef67057c5849af5d98347deaf454a6
Soudly <54641819+Hurch90@users.noreply.github.com>
2024-05-29 00:14:35 +0400

    edit

--- src/main/java/ru/collapsedev/collapseapi/util/NBTUtil.java
+++ src/main/java/ru/collapsedev/collapseapi/util/NBTUtil.java
@@ -5,8 +5,8 @@ import de.tr7zw.nbtapi.NBTItem;
 import lombok.experimental.UtilityClass;
 import org.bukkit.inventory.ItemStack;
 
-@UtilityClass
 // Разбирайся сам со своей хуйнёй (мультиверсия блять)
+@UtilityClass
 public class NBTUtil {
 
     public static ItemStack addNBTTag(ItemStack item, String key, String value) {
