edf00015a1b633c830258aa33e399b847919854a
Underly0 <Vladislav7269@yandex.ru>
2024-09-19 08:35:29 +0300

    add access modifier

--- src/main/java/ru/collapsedev/collapseapi/common/object/MapAccessor.java
+++ src/main/java/ru/collapsedev/collapseapi/common/object/MapAccessor.java
@@ -1,12 +1,15 @@
 package ru.collapsedev.collapseapi.common.object;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 import ru.collapsedev.collapseapi.util.ObjectUtil;
 
 import java.util.List;
 import java.util.Map;
 
 @Getter
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class MapAccessor {
     Map<?, ?> map;
 
